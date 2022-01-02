c95c577d5c840a50bef093461787b43a74f14a4f
LeanLescano <tatilescano11@gmail.com>
2022-01-01 21:50:34 -0300

    Removed unused import of next/link

--- components/CardGame.tsx
+++ components/CardGame.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-import Link from "next/Link";
+import { faLock, faUser } from "@fortawesome/free-solid-svg-icons";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser, faLock } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
 
 interface GameRoom {
   listUsers: [];
