6afee3bd726178e7735831e504bb32ef85184dd3
LeanLescano <tatilescano11@gmail.com>
2022-08-29 23:35:03 -0300

    [fix] - Script tag moved outside Head

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,6 +9,11 @@ type Props = {
 function Layout({ children }: Props) {
   return (
     <>
+            <Script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0"
+          crossOrigin="anonymous"
+        />
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Click battle</title>
@@ -18,11 +23,6 @@ function Layout({ children }: Props) {
           integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl"
           crossOrigin="anonymous"
         ></link>
-        <Script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
-          integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0"
-          crossOrigin="anonymous"
-        />
         <meta
           name="description"
           content="Online multiplayer Click battle - Challenge your friends to a 10 second click battle!"
