90c07b7f5254c379a0033fa9b2dee84bfd3b8557
LeandroLescano <leandrolescano11@gmail.com>
2023-10-15 03:25:33 -0300

    add next.config.js with .env references and change overflow scroll to overflow-y auto to whole app

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: false,
@@ -5,15 +6,15 @@ module.exports = {
     unoptimized: true
   },
   env: {
-    apiKey: "AIzaSyBcEJHGsplG6KemwQ3XbPlxaQZbAEVce0c",
-    authDomain: "click-battle-mp.firebaseapp.com",
-    databaseURL: "https://click-battle-mp-default-rtdb.firebaseio.com",
-    projectId: "click-battle-mp",
-    storageBucket: "click-battle-mp.appspot.com",
-    messagingSenderId: "55439914661",
-    appId: "1:55439914661:web:5877e7ba22e1dcc0b1ff4c",
-    measurementId: "G-ETTEKV9L5B",
-    REACT_APP_EMAIL: "tatilescano11@gmail.com",
-    REACT_APP_PASSWORD: "nrzesscilivmlzbq"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID,
+    REACT_APP_EMAIL: process.env.REACT_APP_EMAIL,
+    REACT_APP_PASSWORD: process.env.REACT_APP_PASSWORD
   }
 };
