82aab3795c22ba3e5b199a859622a7ded00a81a5
LeandroLescano <leandrolescano11@gmail.com>
2023-10-15 03:14:35 -0300

    credentials and gitignore

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: false,
+  images: {
+    unoptimized: true
+  },
+  env: {
+    apiKey: "AIzaSyBcEJHGsplG6KemwQ3XbPlxaQZbAEVce0c",
+    authDomain: "click-battle-mp.firebaseapp.com",
+    databaseURL: "https://click-battle-mp-default-rtdb.firebaseio.com",
+    projectId: "click-battle-mp",
+    storageBucket: "click-battle-mp.appspot.com",
+    messagingSenderId: "55439914661",
+    appId: "1:55439914661:web:5877e7ba22e1dcc0b1ff4c",
+    measurementId: "G-ETTEKV9L5B",
+    REACT_APP_EMAIL: "tatilescano11@gmail.com",
+    REACT_APP_PASSWORD: "nrzesscilivmlzbq"
+  }
+};
