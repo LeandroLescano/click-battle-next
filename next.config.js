8b99273c67aa71c4c13b2f4bc9770ad98abe42c4
Leandro Lescano <leandrolescano11@gmail.com>
2024-03-10 11:45:02 -0300

    change rollbar to sentry

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
+const {withSentryConfig} = require("@sentry/nextjs");
+
 /* eslint-disable no-undef */
 /** @type {import('next').NextConfig} */
 module.exports = {
@@ -16,8 +19,46 @@ module.exports = {
     measurementId: process.env.MEASUREMENT_ID,
     REACT_APP_EMAIL: process.env.REACT_APP_EMAIL,
     REACT_APP_PASSWORD: process.env.REACT_APP_PASSWORD,
-    NEXT_PUBLIC_ROLLBAR_CLIENT_TOKEN:
-      process.env.NEXT_PUBLIC_ROLLBAR_CLIENT_TOKEN,
-    ROLLBAR_SERVER_TOKEN: process.env.ROLLBAR_SERVER_TOKEN
+    SENTRY_AUTH_TOKEN: process.env.SENTRY_AUTH_TOKEN
   }
 };
+
+// Injected content via Sentry wizard below
+
+module.exports = withSentryConfig(
+  module.exports,
+  {
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options
+
+    // Suppresses source map uploading logs during build
+    silent: true,
+    org: "leandro-lescano",
+    project: "click-battle"
+  },
+  {
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
+
+    // Transpiles SDK to be compatible with IE11 (increases bundle size)
+    transpileClientSDK: true,
+
+    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+    tunnelRoute: "/monitoring",
+
+    // Hides source maps from generated client bundles
+    hideSourceMaps: true,
+
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
+
+    // Enables automatic instrumentation of Vercel Cron Monitors.
+    // See the following for more information:
+    // https://docs.sentry.io/product/crons/
+    // https://vercel.com/docs/cron-jobs
+    automaticVercelMonitors: true
+  }
+);
