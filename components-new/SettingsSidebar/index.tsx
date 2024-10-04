7fdee85931e54a258df70e22db6340c678dc9f1b
Leandro Lescano <leandrolescano11@gmail.com>
2024-10-04 20:58:31 -0300

    change styles of settings sidebar

--- components-new/SettingsSidebar/index.tsx
+++ components-new/SettingsSidebar/index.tsx
@@ -109,91 +109,92 @@ export const SettingsSidebar = ({
   };
 
   return (
-    <aside className={clsx("sidebar fixed md:static", {active: showSideBar})}>
-      <div className="flex justify-between items-center">
-        <h2 className="font-extrabold text-primary-300 text-2xl md:text-5xl mb-3 md:mb-6">
-          {t("Settings")}
-        </h2>
-        <Cross
-          className="absolute top-11 right-8 md:right-4 size-8"
-          onClick={() => handleSideBar(false)}
-        />
-      </div>
-      <div className="flex flex-col justify-between gap-2 md:gap-4">
-        <Input
-          label={t("Room name")}
-          labelClassname="text-primary-500 dark:text-primary-200 text-xs md:text-lg"
-          type="text"
-          className="h-9 md:h-12 text-xs md:text-lg mb-2"
-          containerClassName="flex-1"
-          data-label="Room name"
-          value={settings.roomName ?? ""}
-          onChange={(ref) =>
-            setSettings({...settings, roomName: ref.target.value})
-          }
-        />
-        <div>
+    <aside className={clsx("sidebar", {active: showSideBar})}>
+      <div className="px-10 py-12 md:px-20 md:pt-24 min-w-96 md:min-w-[470px]">
+        <div className="flex justify-between items-center mb-3 md:mb-6">
+          <h2 className="font-extrabold text-primary-300 text-2xl md:text-5xl">
+            {t("Settings")}
+          </h2>
+          <Cross className="size-8" onClick={() => handleSideBar(false)} />
+        </div>
+        <div className="flex flex-col justify-between gap-2 md:gap-4">
           <Input
-            label={options.password ? t("Change password") : t("Set password")}
+            label={t("Room name")}
             labelClassname="text-primary-500 dark:text-primary-200 text-xs md:text-lg"
-            type="password"
+            type="text"
             className="h-9 md:h-12 text-xs md:text-lg mb-2"
             containerClassName="flex-1"
-            data-label="Password"
+            data-label="Room name"
+            value={settings.roomName ?? ""}
             onChange={(ref) =>
-              setSettings({...settings, password: ref.target.value})
+              setSettings({...settings, roomName: ref.target.value})
             }
-            placeholder={t("Password")}
           />
-          {options.password && (
-            <label
-              className="underline cursor-pointer"
-              onClick={handleDeletePassword}
-            >
-              {t("Delete password")}
-            </label>
-          )}
+          <div>
+            <Input
+              label={
+                options.password ? t("Change password") : t("Set password")
+              }
+              labelClassname="text-primary-500 dark:text-primary-200 text-xs md:text-lg"
+              type="password"
+              className="h-9 md:h-12 text-xs md:text-lg mb-2"
+              containerClassName="flex-1"
+              data-label="Password"
+              onChange={(ref) =>
+                setSettings({...settings, password: ref.target.value})
+              }
+              placeholder={t("Password")}
+            />
+            {options.password && (
+              <label
+                className="underline cursor-pointer"
+                onClick={handleDeletePassword}
+              >
+                {t("Delete password")}
+              </label>
+            )}
+          </div>
+          <Select
+            label={t("Max number of users")}
+            labelColor="text-primary-500 dark:text-primary-200 text-xs md:text-lg"
+            className="mb-2 h-9 md:h-12 text-xs md:text-lg"
+            containerClassName="flex-1"
+            data-label="Max number of users"
+            value={settings.maxUsers}
+            onChange={(ref) =>
+              setSettings({...settings, maxUsers: Number(ref.target.value)})
+            }
+          >
+            {[...Array.from(range(2, config.maxUsers + 1))].map((val, i) => (
+              <option key={i} value={val}>
+                {val}
+              </option>
+            ))}
+          </Select>
+          <Select
+            label={t("Timer")}
+            labelColor="text-primary-500 dark:text-primary-200 text-xs md:text-lg"
+            className="mb-2 h-9 md:h-12 text-xs md:text-lg"
+            containerClassName="flex-1"
+            data-label="Timer"
+            value={settings.timer}
+            onChange={(ref) =>
+              setSettings({...settings, timer: Number(ref.target.value)})
+            }
+          >
+            {AVAILABLE_TIMES.map((val, i) => (
+              <option key={i} value={val}>
+                {val}
+              </option>
+            ))}
+          </Select>
+          <Button
+            className="w-full py-2 px-3 md:py-4 md:px-5 text-base md:text-3xl mt-4"
+            onClick={handleUpdateSettings}
+          >
+            {t("Save settings")}
+          </Button>
         </div>
-        <Select
-          label={t("Max number of users")}
-          labelColor="text-primary-500 dark:text-primary-200 text-xs md:text-lg"
-          className="mb-2 h-9 md:h-12 text-xs md:text-lg"
-          containerClassName="flex-1"
-          data-label="Max number of users"
-          value={settings.maxUsers}
-          onChange={(ref) =>
-            setSettings({...settings, maxUsers: Number(ref.target.value)})
-          }
-        >
-          {[...Array.from(range(2, config.maxUsers + 1))].map((val, i) => (
-            <option key={i} value={val}>
-              {val}
-            </option>
-          ))}
-        </Select>
-        <Select
-          label={t("Timer")}
-          labelColor="text-primary-500 dark:text-primary-200 text-xs md:text-lg"
-          className="mb-2 h-9 md:h-12 text-xs md:text-lg"
-          containerClassName="flex-1"
-          data-label="Timer"
-          value={settings.timer}
-          onChange={(ref) =>
-            setSettings({...settings, timer: Number(ref.target.value)})
-          }
-        >
-          {AVAILABLE_TIMES.map((val, i) => (
-            <option key={i} value={val}>
-              {val}
-            </option>
-          ))}
-        </Select>
-        <Button
-          className="w-full py-2 px-3 md:py-4 md:px-5 text-base md:text-3xl mt-4"
-          onClick={handleUpdateSettings}
-        >
-          {t("Save settings")}
-        </Button>
       </div>
     </aside>
   );
